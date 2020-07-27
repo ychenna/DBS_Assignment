f3bbf937b5b4e055ccd329efae58d772c2dd2285
ychenna <48149072+ychenna@users.noreply.github.com>
2020-07-27 16:30:48 +0530

    Add files via upload

---
+++ eureka-naming-server/src/test/java/com/eureka/naming/server/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.naming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
